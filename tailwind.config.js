ab7c261ab4721ff4f86d0002751a0063e9c73f8b
Aiman Aisha <aimanaisha851@gmail.com>
2022-01-19 13:54:02 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        myfont:['Rammetto One'],
+        myfont2: ['Poppins'],
+        myfont3: ['Sacramento']
+      },
+      colors:{
+        mycolor1:'#00121c',
+        mycolor2:'#FAB41E',
+        mycolor3:'#010f19',
+      }
+    },
+  },
+  plugins: [],
+}
